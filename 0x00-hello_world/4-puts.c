e2ae0aff73e94206223b91b10ddb11068703edf5
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-10 16:08:28 -0700

    task3&4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * function main is the entry point of the C program
+ * program should print a specific line
+ * program should always return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
