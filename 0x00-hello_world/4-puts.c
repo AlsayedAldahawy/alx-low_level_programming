ef78a5594ad554eafb4caaf9e26c0aca45647cf2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-10 16:12:59 -0700

    task3&4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * function main is the entry point of the C program
- * program should print a specific line
- * program should always return 0
+ * main - function main is the entry point of the C program
+ * 
+ * return: program should always return 0
  */
 int main(void)
 {
