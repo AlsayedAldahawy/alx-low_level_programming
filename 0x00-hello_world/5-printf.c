6c7043e49d190c660a4acda7de9d466d1eae7fc3
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-10 16:22:57 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * simple program to print a specific line
+ * main - the entry point of the program
+ * Return: return the value 0 always if the program worked successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
