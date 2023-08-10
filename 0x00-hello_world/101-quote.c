a8a72d04e03bbfbc9c144cb00ea1d473b0271eb4
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-10 16:51:24 -0700

    task_101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 /**
  * main - the entry point of the program that prints a specific quote.
  * Return: returns the value (1) when successfully executed
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
