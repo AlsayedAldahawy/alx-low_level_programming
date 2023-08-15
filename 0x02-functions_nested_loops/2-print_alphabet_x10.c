92cd8a111b1f60f931ed8e4606b59f4e3bc7cf91
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-15 20:24:45 +0300

    print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function prints all the lowercase alphabets 10 times.
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        print_alphabet();
+    }
+}
