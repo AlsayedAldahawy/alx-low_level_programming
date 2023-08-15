c020f1312be49abbfd6634fa286b2cc6778ebe7f
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-15 22:51:01 +0300

    jack_bauer.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ */
+void times_table(void)
+{
+	int x = 0, y = 0, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if ((z / 10) > 0)
+			{
+				_putchar(((z / 10) % 10) + '0');
+			}
+			_putchar((z % 10) + '0');
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if ((z / 10) <= 0)
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+
+}
