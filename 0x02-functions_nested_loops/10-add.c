0791c9799a0b23e4366e9fb55a56a175cf8b4043
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-15 23:32:00 +0300

    add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - adds two integers and returns the result.
  * @x: 1st argument value
  * @y: 2nd argument value
- * return: returns the addition value of the two arguments
+ * Return: returns the addition value of the two arguments
  */
 int add(int x, int y)
 {
