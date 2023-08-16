65f0d10586927e5a7b7c74208faa0cfbb17a99c1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-16 04:58:09 +0300

    fibonacci3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,33 +7,37 @@
 
 int main(void)
 {
-	unsigned long f0 = 1, f1 = 2, fn;
+	unsigned long int A, B, A1, A2, B1, B2;
 	int i;
 
-	for (i = 0; i < 100; i++)
+	A = 1;
+	B = 2;
+
+	printf("%lu", A);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (i == 0)
-		{
-			fn = f0;
-		}
-		else if (i == 1)
-		{
-			fn = f1;
-		}
-		else
-		{
-			fn = f1 + f0;
-			f0 = f1;
-			f1 = fn;
-		}
-
-		printf("%lu", fn);
-
-		if (i < 99)
-		{
-			printf(", ");
-		}
+		printf(", %lu", B);
+		B = B + A;
+		A = B - A;
 	}
+
+	A1 = A / 1000000000;
+	A2 = A % 1000000000;
+	B1 = B / 1000000000;
+	B2 = B % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", B1 + (B2 / 1000000000));
+		printf("%lu", B2 % 1000000000);
+		B1 = B1 + A1;
+		A1 = B1 - A1;
+		B2 = B2 + A2;
+		A2 = B2 - A2;
+	}
+
 	printf("\n");
+
 	return (0);
 }
