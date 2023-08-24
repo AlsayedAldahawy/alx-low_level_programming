b5c8e73dc8d43ba418090549c9de6a02f744eb95
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-24 07:20:18 +0300

    print int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: number to be printed.
+ */
+void print_number(int n)
+{
+	/**
+	 * using recursion
+	*/
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
