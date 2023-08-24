48a072924647a6454e275faabfdf4306191f5a36
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-24 06:00:34 +0300

    ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,18 +11,17 @@ char *rot13(char *s)
 {
 	int i = 0, j;
 
-	char *rot_1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot_2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot_1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot_2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 
 	while (s[i] != '\n')
 	{
-		for (j = 0; j != '\0'; j++)
+		for (j = 0; rot_1[j] != '\0'; j++)
 		{
 			if (s[i] == rot_1[j])
 			{
 				s[i] = rot_2[j];
-				break;
 			}
 		}
 		i++;
