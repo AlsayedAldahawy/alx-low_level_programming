1ff833c6fa4ab8c69e920e385acfe9a3cbda1889
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-24 06:08:25 +0300

    ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,13 +15,14 @@ char *rot13(char *s)
 	char rot_2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 	{
 		for (j = 0; rot_1[j] != '\0'; j++)
 		{
 			if (s[i] == rot_1[j])
 			{
 				s[i] = rot_2[j];
+				break;
 			}
 		}
 		i++;
