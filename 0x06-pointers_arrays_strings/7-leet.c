beb3871e25b435b0ef7417d265c6f778cdc0a0f4
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-24 05:18:23 +0300

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *s)
 {
 	int i = 0, j;
 
-	char let[10] = {'a', 'A', 'e', 'E', 'o', 'O', 'i', 'I', 'l', 'L'};
+	char let[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char let_n[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	while (s[i] != '\n')
