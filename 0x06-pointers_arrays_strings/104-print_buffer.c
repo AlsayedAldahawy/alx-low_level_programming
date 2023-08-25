3f065fafd8d916869dfcf050da7b4552d5a9992f
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-08-25 04:44:40 +0300

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,41 +13,41 @@ void print_buffer(char *b, int size)
 	int i = 0, j = 10;
 	int size_var = size;
 
-	while (i <= size)
+	if (size == 0)
+		putchar('\n');
+	else
 	{
-		if (i == size)
+		while (i <= size)
 		{
-			putchar('\n');
-			break;
-		}
-		if (i % 10 == 0)
-		{
-			printf("%08x: ", i);
-
-			for (j = 1; j <= 10; j++)
+			if (i == size)
 			{
-
-				if (size_var <= 0)
-				{
-					putchar(' ');
-					putchar(' ');
-				}
-				else
-					printf("%02x", b[i + j - 1]);
-
-				if (j % 2 == 0)
-					putchar(' ');
-				size_var--;
+				putchar('\n');
+				break;
 			}
+			if (i % 10 == 0)
+			{
+				printf("%08x: ", i);
+				for (j = 1; j <= 10; j++)
+				{
+					if (size_var <= 0)
+					{
+						putchar(' ');
+						putchar(' ');
+					}
+					else
+						printf("%02x", b[i + j - 1]);
+					if (j % 2 == 0)
+						putchar(' ');
+					size_var--;
+				}
+			}
+			if (b[i] <= 31 || b[i] >= 127)
+				putchar('.');
+			else
+				putchar(b[i]);
+			if ((i + 1) % 10 == 0)
+				putchar('\n');
+			i++;
 		}
-
-		if (b[i] <= 31 || b[i] >= 127)
-			putchar('.');
-		else
-			putchar(b[i]);
-
-		if ((i + 1) % 10 == 0)
-			putchar('\n');
-		i++;
 	}
 }
