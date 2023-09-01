254d04d25870a02f3f13880d11cb4f2ba870bcd1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-01 23:07:33 +0300

    argc_argv_0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ *
+ * @argc: number of command line arguments.
+ * @argv: array of strings that holds the arguments values.
+ *
+ * return: always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+    (void) argc;
+	printf("%s\n", argv[0]);
+    return (0);
+}
