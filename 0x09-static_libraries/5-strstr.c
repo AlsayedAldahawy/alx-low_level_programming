b442ae701cb1c1e7407f17a0d155e5946875965d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-01 17:39:09 +0300

    static library 1

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: string to be scanned.
+ * @needle: string containing the characters to find.
+ * Return: Returns a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j, k;
+
+	while (haystack[i])
+	{
+		for (j = 0, k = i; needle[j]; j++, k++)
+		{
+			if (haystack[k] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (haystack + i);
+		i++;
+	}
+	return (NULL);
+}
