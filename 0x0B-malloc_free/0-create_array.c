4dacc41e8c1cc9e89225f2bb1080342c0630e493
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-05 19:25:10 +0300

    malloc-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *ptr;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = malloc(size * sizeof(char));
 
 	for (i = 0; i <= size; i++)
