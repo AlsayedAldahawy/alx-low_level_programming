6d407124eae45e23f29a59ae52cbd49b90c5eb15
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-05 21:31:41 +0300

    malloc-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,16 +22,23 @@ char *str_concat(char *s1, char *s2)
 	int i, j, size1 = 0, size2 = 0;
 	char *ptr;
 
-	if (s1 != NULL)
+	if (s1 == NULL)
+	{
+		size1 = 0;
+	}
+	else
 	{
 		while (s1[size1] != '\0')
 		{
 			size1++;
 		}
 	}
-	if (s2 != NULL)
+	if (s2 == NULL)
 	{
-		size2 = 0;
+		size2 = 1;
+	}
+	else
+	{
 		while (s2[size2] != '\0')
 		{
 			size2++;
@@ -40,18 +47,15 @@ char *str_concat(char *s1, char *s2)
 	ptr = malloc(size1 + size2 + 1);
 	if (ptr == NULL)
 		return (NULL);
-
 	for (i = 0; i < size1; i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = i; j <= size1 + size2 + 1; j++)
+	for (j = i; j < size1 + size2 + 1; j++)
 	{
 		ptr[j] = s2[j - i];
 		if (ptr[j] == '\0')
-		{
 			return (ptr);
-		}
 	}
 	return (NULL);
 }
