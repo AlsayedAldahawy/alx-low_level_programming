49816ddeb201c85472cb5172461d624fb2acee0f
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-07 20:16:20 +0300

    malloc_C_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -46,7 +46,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 	}
 
-	for (j = 0; i < size - 1; j++, i++)
+	size--;
+	for (j = 0; i < size; j++, i++)
 	{
 		ptr[i] = s2[j];
 	}
