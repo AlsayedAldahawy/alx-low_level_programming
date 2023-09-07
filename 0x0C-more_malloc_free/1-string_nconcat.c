de0d28a64631498642b852f07296e054b3a5cbab
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-07 18:15:12 +0300

    malloc_C_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,8 +8,10 @@
  *
  * @s1: string 1.
  * @s2: string 2.
+ * @n: number of letters used from S2
  *
- * Return: The returned pointer shall point to a newly allocated space in memory,
+ * Return: The returned pointer shall point to
+ * a newly allocated space in memory,
  * which contains s1, followed by the first n bytes of s2, and null terminated
  * If the function fails, it should return NULL
  * If n is greater or equal to the length of s2 then use the entire string s2
@@ -18,35 +20,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *ptr;
-    long unsigned int size, i, j;
+	char *ptr;
+	unsigned long int size, i, j;
 
-    if (n >= sizeof s2)
-    {
-        size = strlen(s1) + strlen(s2) + 1;
-    }
-    else
-    {
-        size = strlen(s1) + strlen(s2) + 1;
-    }
+	if (n >= strlen(s2))
+	{
+		size = strlen(s1) + strlen(s2) + 1;
+	}
+	else
+	{
+		size = strlen(s1) + strlen(s2) + 1;
+	}
 
-    ptr = malloc(strlen(s1) + strlen(s2) + 1);
-        if (ptr == NULL)
-            return (NULL);
+	ptr = malloc(strlen(s1) + strlen(s2) + 1);
+		if (ptr == NULL)
+			return (NULL);
 
-    for (i = 0, j = 0; i < size; i++)
-    {
-        if (i < strlen(s1))
-        {
-            ptr[i] = s1[i];
-        }
-        else
-        {
-            ptr[i] = s2[j];
-            j++;
-        }
-    }
-    ptr[size] = 0;
-    
-    return(ptr);
+	for (i = 0, j = 0; i < size; i++)
+	{
+		if (i < strlen(s1))
+		{
+			ptr[i] = s1[i];
+		}
+		else
+		{
+			ptr[i] = s2[j];
+			j++;
+		}
+	}
+	ptr[size] = 0;
+	return (ptr);
 }
