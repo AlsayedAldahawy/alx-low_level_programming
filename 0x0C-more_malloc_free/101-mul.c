9010a8c4b7540204bc0630eaaafa26d2eaa9a1f9
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-08 05:02:10 +0300

    malloc_C_101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,6 +5,7 @@
 
 unsigned long int ston(char *s);
 int isnum(char s[]);
+int _strlen(char *s);
 
 /**
  * main -a program that multiplies two positive numbers.
@@ -15,7 +16,7 @@ int isnum(char s[]);
  * Return: 0 when success.
  * Usage: mul num1 num2
  * num1 and num2 will be passed in base 10
- * Print the result, followed by a new line
+ * Print the res, followed by a new line
  *
  * *****If the number of arguments is incorrect, print Error,
  * followed by a new line, and exit with a status of 98******
@@ -28,12 +29,49 @@ int isnum(char s[]);
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || (!(isnum(argv[1]) && isnum(argv[2]))))
+	int *res, len_1, len_2, len_r, i, d1 = 0, d2 = 0, carry, frstD = 0;
+	char *str1 = argv[1], *str2 = argv[2];
+
+	if (argc != 3 || (!(isnum(str1) && isnum(str2))))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	printf("%lu\n", ston(argv[1]) * ston(argv[2]));
+
+	len_1 = _strlen(str1);
+	len_2 = _strlen(str2);
+
+	len_r = len_1 + len_2 + 1;
+	res = malloc(sizeof(int) * len_r);
+	if (!res)
+		return (1);
+	for (i = 0; i <= len_1 + len_2; i++)
+		res[i] = 0;
+	for (len_1 = len_1 - 1; len_1 >= 0; len_1--)
+	{
+		d1 = str1[len_1] - 48;
+		carry = 0;
+		for (len_2 = _strlen(str2) - 1; len_2 >= 0; len_2--)
+		{
+			d2 = str2[len_2] - 48;
+			carry += res[len_1 + len_2 + 1] + (d1 * d2);
+			res[len_1 + len_2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			res[len_1 + len_2 + 1] += carry;
+	}
+	for (i = 0; i < len_r - 1; i++)
+	{
+		if (res[i])
+			frstD = 1;
+		if (frstD)
+			_putchar(res[i] + 48);
+	}
+	if (!frstD)
+		_putchar(48);
+	_putchar('\n');
+	free(res);
 	return (0);
 }
 
@@ -88,4 +126,24 @@ int isnum(char s[])
 	}
 
 	return (1);
+}
+
+/**
+ * _strlen - a function that returns the length of a string.
+ *
+ * @s: string passed by reference.
+ *
+ * Return: returns the length of the string.
+ *
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
