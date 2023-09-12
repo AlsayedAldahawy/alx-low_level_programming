e3d54cacffd1da2c251df99ff4c9ad594fc10989
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-12 03:28:50 +0300

    Dog_2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function that prints a struct dog.
+ * @d: pointer to struct.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
