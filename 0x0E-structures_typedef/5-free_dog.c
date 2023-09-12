61183c27965b9da20844ec80c1909a7a2562db17
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-12 04:30:56 +0300

    Dog_free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog -  function that frees dogs.
+ * @d: pointer to a dog.
+ */
+void free_dog(dog_t *d)
+{
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
