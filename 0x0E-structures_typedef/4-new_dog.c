79e05fbd5822b1e50e00cccfc4ed26217ec9f879
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-12 04:23:13 +0300

    Dog_4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -38,8 +38,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	p->name = name;
-	p->owner = owner;
+	strcpy(p->name, name);
+	strcpy(p->owner, owner);
 	p->age = age;
 
 	return (p);
