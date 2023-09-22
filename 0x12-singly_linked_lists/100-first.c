35a8eacf2e3fde079853d5e19b7a30d013329796
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-22 05:29:06 +0300

    singly linked lists 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,14 @@
 #include "lists.h"
 
-void first(void) __attribute__ ((constructor));
-
-
 /**
- * free_list - a function that prints
- * "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ * constructor_function - a function that prints
+ * "You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n"
  * before the main function is executed.
- * @head: pointer to the linked list
  */
 
-void first(void)
+void constructor_function(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
