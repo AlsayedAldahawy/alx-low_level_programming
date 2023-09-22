9ed122121de236b753caefcb5cd497c84e61d410
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-22 05:40:50 +0300

    singly linked lists 101

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,6 +4,7 @@
  * list_len - a function that returns the number of elements
  * in a linked list_t list.
  * @h: pointer to the linked list
+ * Return: number of elements.
  */
 
 size_t list_len(const list_t *h)
