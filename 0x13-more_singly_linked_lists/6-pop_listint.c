6bfddf9d6a1c3ab95ce7f4fc95dbf2c14925002e
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-26 15:20:52 +0300

    more singly linked list 7

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *ptr = (*head)->next;
-	int x = (*head)->n;
+	listint_t *ptr;
+	int num;
 
 	if (!head || !*head)
 		return (0);
 
+	num = (*head)->n;
+	ptr = (*head)->next;
 	free(*head);
 	*head = ptr;
 
-	return (x);
+	return (num);
 }
