92d8a4178f6963c5533df5b73b6702148f68f082
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-26 06:35:25 +0300

    more singly linked list 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint -  a function that deletes the head node of a listint_t
+ * linked list, and returns the head node’s data (n).
+ * @head: pointer to singly linked list head.
+ * Return: returns the head node’s data (n)
+ * if the linked list is empty return 0.
+ */
+int pop_listint(listint_t **head)
+{
+	int x = (*head)->n;
+	listint_t *ptr = (*head)->next;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	free(*head);
+	*head = ptr;
+
+	return (x);
+}
