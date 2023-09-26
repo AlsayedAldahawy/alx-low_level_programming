1c835d8c7d1600641e149320e60346582419ef05
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-27 01:58:16 +0300

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	ptr = *head;
-
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (i < idx - 1)
 	{
 		if (!ptr)
