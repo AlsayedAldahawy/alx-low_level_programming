9d3c3d03b0ab78e8ea903d8b29d88e6dc3060303
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-02-08 20:56:31 +0200

    rev-c0103

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,8 @@
 /**
  * main - entry point of the code
  * Return: 0 at success.
- * prints all lowercase letters
+ * a program that prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
  */
 
 int main(void)
@@ -11,6 +12,9 @@ int main(void)
 	char i = 'a';
 
 	while (i <= 'z')
+		putchar(i++);
+	i = 'A';
+	while (i <= 'Z')
 		putchar(i++);
 	putchar('\n');
 	return (0);
