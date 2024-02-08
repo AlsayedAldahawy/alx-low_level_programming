8af76b8727a24bb1332f21b45501f097db4eb4be
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-02-08 21:11:33 +0200

    rev-c0108

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,15 +12,15 @@ int main(void)
 	char i = '0';
 
 	while (i <= '9')
-    {
+	{
 		putchar(i++);
-    }
-    i = 'a';
+	}
+	i = 'a';
 
-    while (i <= 'f')
-    {
-        putchar(i++);
-    }
+	while (i <= 'f')
+	{
+		putchar(i++);
+	}
 	putchar('\n');
 	return (0);
 }
