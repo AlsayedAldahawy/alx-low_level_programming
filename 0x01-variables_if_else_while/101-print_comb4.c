36b970a6ec8c39d08453abbd08d14adee129a7d7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-02-08 21:41:23 +0200

    rev-c0111

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ * Return: 0 at success.
+ * a program that prints all possible different combinations of two digits.
+ */
+
+int main(void)
+{
+	int i = '0', j, k;
+
+	while (i <= '8')
+	{
+		j = i + 1;
+		while (j <= '9')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
