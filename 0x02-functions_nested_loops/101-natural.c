f2370f6c625b38a993ec1938fc35dbbd1ad8ae94
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-02-10 00:44:40 +0200

    rev-c0213

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+*/
+
+int main (void)
+{
+    int sum = 0, i = 0;
+
+   while (i < 1024)
+   {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+        i++;
+   }
+   printf("%i\n", sum);
+   return (0);
+}
