ee12ee33a8622227590828e97362a406b8dd9bea
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-13 02:29:14 +0200

    rev-c0612

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 /**
+ * printLine - prints a buffer
+ * @b: pointer to buffer to be printed
+ * @size: size of buffer
+ * @line: line of buffer to print
+ */
+
+void printLine(int line, int size, char *b);
+
+/**
  * print_buffer - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
@@ -9,43 +18,57 @@
 
 void print_buffer(char *b, int size)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
+
 	while (i < size)
 	{
 		printf("%08x: ", i);
-
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j >= size)
-			{
-				printf("  ");
-			}
-			else
-				printf("%02x", b[i + j]);
-			if (!((j + 1) % 2))
-				printf(" ");
-		}
-
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j >= size)
-			{
-				break;
-			}
-			if (b[i + j]  <= 31 || b[i + j] >= 127)
-			{
-				printf(".");
-			}
-			else
-				printf("%c", b[i + j]);
-		}
+		printLine(i, size, b);
 		printf("\n");
 		i += 10;
 	}
+}
+
+/**
+ * printLine - prints a buffer
+ * @b: pointer to buffer to be printed
+ * @size: size of buffer
+ * @line: line of buffer to print
+ */
+
+void printLine(int line, int size, char *b)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (line + j >= size)
+		{
+			printf("  ");
+		}
+		else
+			printf("%02x", b[line + j]);
+		if (!((j + 1) % 2))
+			printf(" ");
+	}
+
+	for (j = 0; j < 10; j++)
+	{
+		if (line + j >= size)
+		{
+			break;
+		}
+		if (b[line + j]  <= 31 || b[line + j] >= 127)
+		{
+			printf(".");
+		}
+		else
+			printf("%c", b[line + j]);
+		}
 }
