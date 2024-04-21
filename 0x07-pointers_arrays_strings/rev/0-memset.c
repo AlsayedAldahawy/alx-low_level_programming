ede9de832331a1a5d704aa74bcbce7da962fe35d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-21 03:36:01 +0200

    rev-c0700

--- 0x07-pointers_arrays_strings/rev/0-memset.c
+++ 0x07-pointers_arrays_strings/rev/0-memset.c
@@ -12,13 +12,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i = 0;
 
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		s[i] = b;
+		*(s + i) = b;
+		i++;
 	}
-
 	return (s);
 }
