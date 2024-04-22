89e769feb5adcc5ec0a72276b5a0efcd324095f3
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-22 20:13:18 +0200

    rev-c0809

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,28 +16,25 @@
 
 int wildcmp(char *s1, char *s2)
 {
+	/* Base case: both strings are empty */
 	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
 
-	if (*s1 == '\0' && *(s2 + 1) != '\0' && *s2 == '*')
+	/* If s2 starts with '*', recursively check if the rest of s1 and s2 match */
+	if (*s2 == '*')
 	{
-		return (0);
+		/*Try matching s1 with and without the first character of s2*/
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 
-	if (*s1 == *s2)
+	/*If s2 does not start with '*', compare the first characters of s1 and s2*/
+	if (*s1 != '\0' && *s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 
-	if (*s2 == '*')
-	{
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-		{
-			return (1);
-		}
-	}
-
+	/*If none of the above conditions match, the strings are not identical*/
 	return (0);
 }
