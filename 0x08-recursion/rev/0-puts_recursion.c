3a1514796402269991a93c1ee7c328d928293aed
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-22 04:16:56 +0200

    rev-c0800

--- 0x08-recursion/rev/0-puts_recursion.c
+++ 0x08-recursion/rev/0-puts_recursion.c
@@ -5,15 +5,14 @@
  *
  * @s: pointer to character.
  */
-
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 
-	_putchar(s[0]);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
