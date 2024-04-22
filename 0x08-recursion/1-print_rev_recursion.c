2d1b3c8a1c6e8d29a5146195f87f56c876b2a8b0
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-22 04:35:58 +0200

    rev-c0802

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,6 +11,11 @@ void _print_rev_recursion(char *s)
 	if (*s == '\0')
 		return;
 
+	/**
+	 * this time we wan't to print in reverse, so the last letter
+	 * should be printed before the next to the last .. etc
+	 * so we use the recursion before _putchar
+	*/
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
