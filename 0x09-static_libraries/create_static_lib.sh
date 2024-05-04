486995803602548c805470266e058ed54afae1f2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-04 17:42:33 +0300

    rev-c0900

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-gcc -c -Wall -Werror -pedantic -- *.c
-ar -rc liball.a -- *.o
+#!/usr/bin/env bash
+# shellcheck disable=SC2035
+
+gcc -c -Wall -Werror -pedantic *.c
+ar -rc liball.a *.o
 ranlib libmy.a 
