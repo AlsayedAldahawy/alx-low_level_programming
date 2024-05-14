5b7c896c603b9609913e6013862857f335a1efbc
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-15 02:05:24 +0300

    rev-C-0B06

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,91 +2,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int wordsCounter(char *str);
+int wordCounter(char *str);
 
 /**
- * strtow - Splits a string into words.
- * @str: The input string to split.
+ * strtow - a function that splits a string into words.
+ * @str: string to be splitted.
  *
- * Return: A pointer to an array of strings (words), or NULL on failure.
+ * Return: The function returns a pointer to an array of strings (words)
+ * Each element of this array should contain a single word, null-terminated
+ * The last element of the returned array should be NULL
+ * Words are separated by spaces
+ * Returns NULL if str == NULL or str == ""
+ * If your function fails, it should return NULL
  */
+
 char **strtow(char *str)
 {
+	int i = 0, j = 0, words = 0, letters = 0, k = 0, n = 0, s;
 	char **arr;
-	int i = 0, j = 0, k, l, m = 0, words, letters = 0;
 
-	if (!str || !*str)
-		return (NULL);
-
-	words = wordsCounter(str);
-
-	/* Allocate memory for pointers to each word and the NULL */
-	arr = malloc(sizeof(char *) * (words + 1));
-
-	if (!arr)
-		return (NULL);
-
-	while (str[i])
+	words = wordCounter(str);
+	if (str == NULL || *str == '\0' || !words)
 	{
-		if (str[i] == 32 && str[i - 1] == 32)
-		{
-			i++;
-			continue;
-		}
-
-		letters++;
-
-		if (str[i] == 32 || !str[i + 1])
-		{
-			if (!str[i + 1])
-				m = 1;
-
-			/* Allocate memory for each word */
-			arr[j] = malloc(sizeof(char) * (letters + m));
-
-			if (!arr[j])
-			{
-				/* Free all allocated memory and return NULL */
-				for (i = 0; i < j; i++)
-					free(arr[i]);
-				free(arr);
-				return (NULL);
-			}
-
-			l = letters;
-			for (k = 0; k < l - 1 + m; k++)
-			{
-
-				arr[j][k] = str[i - letters + 1];
-				letters--;
-			}
-			arr[j++][k] = '\0';
-			letters = 0;
-		}
-
-		i++;
+		return (NULL);
 	}
-	arr[j] = NULL;
 
+	arr = malloc((words + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < words; i++)
+	{
+		letters = 0;
+		while (str[j])
+		{
+			if (str[j] != ' ')
+			{
+				letters++;
+			}
+			if (str[j] == ' ' && letters > 0)
+			{
+				break;
+			}
+			j++;
+		}
+		arr[i] = malloc((letters * sizeof(char)) + 1);
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (k = 0; k <= i; k++)
+				free(arr[k]);
+			return (NULL);
+		}
+
+		for (k = n, s = 0; k < j; k++)
+		{
+			if (str[k] != ' ')
+			{
+				arr[i][s] = str[k];
+				s++;
+			}
+		}
+		arr[i][s] = '\0';
+		n = j;
+	}
+	arr[i] = NULL;
 	return (arr);
 }
 
 /**
- * wordsCounter - Counts the number of words in a string.
- * @str: The input string.
+ * wordCounter - counts words in a string.
+ * @str: string.
  *
- * Return: The number of words.
+ * Return: number of words.
  */
-int wordsCounter(char *str)
+
+int wordCounter(char *str)
 {
-	int wc = 0, i = 0;
+	int i = 0, words = 0;
 
 	while (str[i])
 	{
-		if ((i && str[i] == 32 && str[i + 1] != 32) || !str[i + 1])
-			wc++;
+		if ((str[i] == 32 && i && str[i - 1] != 32) || (!str[i + 1] && str[i] != 32))
+		{
+			words++;
+		}
 		i++;
 	}
-
-	return (wc);
+	return (words);
 }
