ea08b40cc5b624703536c1ced52e3a45f8bbd540
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-19 01:46:23 +0300

    rev-C-0C04

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -45,7 +45,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	(new_size > old_size) ? (new_size += 0) : (new_size = old_size);
 
 	for (i = 0; i < new_size; i++)
-		ptrNew[i] = ((char *)ptr + i);
+		ptrNew[i] = ((char *)(ptr + i));
 
 	free(ptr);
 	return (ptrNew);
