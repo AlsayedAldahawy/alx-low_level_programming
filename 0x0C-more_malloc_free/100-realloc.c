e2b19c9cabb10a5a8b9f5b1693ded383ae855091
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-19 01:26:46 +0300

    rev-C-0C04

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,29 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
+ * _realloc - a function that reallocates a memory block
+ * using malloc and free
  *
- * @ptr: A pointer to the memory previously allocated with malloc
- * (malloc(old_size)).
- * @old_size: The size, in bytes, of the allocated space for ptr.
- * @new_size: The new size, in bytes, of the new memory block.
  *
- * Return:
- *   - On success, returns a pointer to the newly allocated memory.
- *   - If new_size > old_size, the "added" memory is uninitialized.
- *   - If new_size == old_size, returns ptr without any changes.
- *   - If ptr is NULL, equivalent to malloc(new_size).
- *   - If new_size is zero and ptr is not NULL, equivalent to free(ptr).
+ * @ptr: is a pointer to the memory previously allocated with a
+ * call to malloc: malloc(old_size).
  *
- * FYI: The standard library provides a different function called realloc.
- * Run 'man realloc' to learn more.
+ * @old_size: is the size, in bytes, of the allocated space for ptr.
+ *
+ * @new_size: is the new size, in bytes of the new memory block.
+ * Return: the pointer to the newly created array
+ * The contents will be copied to the newly allocated space,
+ * in the range from the start of ptr up to the minimum of
+ * the old and new sizes
+ *
+ * If new_size > old_size, the “added” memory should not be initialized
+ *
+ * If new_size == old_size do not do anything and return ptr
+ *
+ * If ptr is NULL, then the call is equivalent to malloc(new_size),
+ * for all values of old_size and new_size
+ * If new_size is equal to zero, and ptr is not NULL, then the call
+ * is equivalent to free(ptr). return NULL
+ * Don’t forget to free ptr when it makes sense
+ *
+ *
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr2;
-	unsigned int i;
+	int i, size;
+	char *ptrNew, *ptrOld = ptr;
 
 	if (!ptr)
 		return (malloc(new_size));
@@ -31,22 +42,28 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (!new_size)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	ptr2 = malloc(new_size);
+	if (old_size < new_size)
+		size = old_size;
+	else if (old_size > new_size)
+		size = new_size;
+	else
+		return (ptr);
 
-	if (!ptr2)
+	ptrNew = malloc(new_size);
+	if (!ptrNew)
 		return (NULL);
 
-	(new_size > old_size) ? (new_size += 0) : (new_size = old_size);
-
-	for (i = 0; i < new_size; i++)
-		ptr2[i] = ((char *)ptr)[i];
+	for (i = 0; i < size; i++)
+	{
+		*(ptrNew + i) = *(ptrOld + i);
+	}
 
 	free(ptr);
-	return (ptr2);
+	return (ptrNew);
 }
