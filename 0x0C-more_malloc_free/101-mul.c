66e7bd2112fa85e9b17cae77dc802cc4764403e7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-23 12:51:53 +0300

    0x0C-more_malloc_free update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,7 +15,7 @@
  */
 int isInteger(char *str)
 {
-	unsigned int i = 0, NonZero = 0;
+	unsigned int i = 0;
 	int sign = 1, x = 0;
 
 	if (!str || !str[i])
@@ -33,12 +33,9 @@ int isInteger(char *str)
 		if (str[i] > '9' || str[i] < '0')
 			return (0);
 
-		if (str[i] != '0')
-			NonZero = 1;
-
 		i++;
 	}
-	return (((i * sign) + x) * NonZero);
+	return (((i * sign) + x));
 }
 
 /**
@@ -101,6 +98,27 @@ int leadingZeros(char *str)
 }
 
 /**
+ * isZero - Checks if a string represents the number zero.
+ *
+ * This function determines if the provided string consists only of '0
+ * characters.
+ *
+ * @str: The string to be checked.
+ *
+ * Return: 1 if the string is zero, 0 otherwise.
+ */
+int isZero(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i])
+	{
+		if (str[i++] != '0')
+			return (0);
+	}
+	return (1);
+}
+/**
  * main - Multiplies two positive numbers given as command-line arguments.
  *
  * @argc: The number of command-line arguments.
@@ -123,35 +141,32 @@ int main(int argc, char *argv[])
 	len_2 = isInteger(argv[2]);
 	(len_1 < 0) ? (len_1 *= -1, sign1 = 1) : (0);
 	(len_2 < 0) ? (len_2 *= -1, sign2 = 1) : (0);
-
 	if (argc != 3 || !len_1 || !len_2)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
+	if (isZero(argv[1] + sign1) || isZero(argv[2] + sign2))
+	{
+		printf("0\n");
+		return (0);
+	}
 	LZ1 = leadingZeros(argv[1] + sign1);
 	LZ2 = leadingZeros(argv[2] + sign2);
-
 	len_1 -= LZ1;
 	len_2 -= LZ2;
-
 	ptr = malloc(sizeof(char) * (len_1 + len_2));
-
 	if (!ptr)
 		return (1);
-
 	for (i = 0; i < len_1 + len_2; i++)
 		ptr[i] = '0';
 	skip1 = sign1 + LZ1;
 	skip2 = sign2 + LZ2;
-
 	ptr = intMul(ptr, (argv[1] + skip1), (argv[2] + skip2), len_1, len_2);
 	(sign1 != sign2) ? (_putchar('-')) : (0);
-
+	skip1 = 0;
 	for (i = 0; i < len_1 + len_2; i++)
-		(!i && ptr[i] == '0') ? (i++, _putchar(ptr[i])) : (_putchar(ptr[i]));
-
+		((!skip1 && ptr[i] == '0')) ? (0) : (_putchar(ptr[i]), skip1++);
 	_putchar('\n');
 	free(ptr);
 	return (0);
