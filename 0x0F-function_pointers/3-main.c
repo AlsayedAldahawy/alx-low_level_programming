2db4b3213cda1f25a6923e06bfffd6c1071717b1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-29 15:48:00 +0300

    rev-C-0F03

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,21 +1,12 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
-/**
- * main - entry point
- *
- * @argc: arguments count.
- * @argv: arguments vector.
- * Return: Always 0.
- */
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, res;
+	int (*func)(int, int); /*pointer to function*/
+	int res, num1, num2;
 	char op;
-	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -23,18 +14,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
 
 	if (!func)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(98);
 	}
 
-	op = *argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = *argv[2]; /*derefrencing to the first character of the string*/
 
 	if ((op == '/' || op == '%') & (num2 == 0))
 	{
@@ -45,6 +35,5 @@ int main(int argc, char *argv[])
 	res = func(num1, num2);
 
 	printf("%d\n", res);
-
 	return (0);
 }
